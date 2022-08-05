d5f690186be98e32abc4445d5e1716165f41f4d7
José Antonio Verdugo Ardila <93588018+joseverdugo-dev@users.noreply.github.com>
2022-08-05 13:12:27 +0200

    Add files via upload

---
+++ 02-Sintaxsis/sintaxsis.js
@@ -0,0 +1,12 @@
+const datos = [
+  "Jose Antonio",
+  30,
+  false,
+  new Date(1991, 11, 24),
+  {
+    titulo: "El Señor de los Anillos:La Comunidad del Anillo",
+    autor: "J.R.R. Tolkien",
+    fecha: new Date(1954, 07, 29),
+    url: "https://www.amazon.es/Anillos-Comunidad-Anillo-Biblioteca-Tolkien/dp/8445000667",
+  },
+];
