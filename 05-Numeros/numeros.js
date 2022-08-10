7821a9340f681de3cd5e88ddc1995bb91eb4279d
José Antonio Verdugo Ardila <93588018+joseverdugo-dev@users.noreply.github.com>
2022-08-10 10:54:12 +0200

    Add files via upload

---
+++ 05-Numeros/numeros.js
@@ -0,0 +1,13 @@
+let altura_cm = 184;
+
+let altura_m = 1.84;
+
+let peso_kg = 85.5;
+
+let altura_round = Math.ceil(altura_m);
+console.log(altura_round);
+
+let peso_round = Math.floor(peso_kg);
+console.log(peso_round);
+
+let iguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
